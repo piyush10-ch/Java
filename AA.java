3a1ae589ad227153c7e0deecbcaafd27c0e74654
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-10-02 10:29:24 +0530

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,6 @@
+public class AA {
+    public static void main(String[] args){   \\ THIS IS A SINGL LINE COMMENT 
+        System.out.println("HELLO PIYUSH CHAUDHARY");
+    }/*JFHJNDJGJJGFJFGJJFGJFG FKF
+         THIS IS A DOUBLE LINE COMMENT    */ 
+}
