ed941e723414c15c61acc9db5257834018afbda4
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-10-02 10:30:14 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args){
+        String myname ="PIYUSH CHAUDHARY";
+        int myage = 23;
+        float myfloatheight = 5.11f;
+        boolean mybo= true;
+        System.out.println( "Mr" + myname);   
+        System.out.println(myage);
+        System.out.println(myfloatheight);
+        System.out.println(mybo) ;
+
+    }
+    
+}
