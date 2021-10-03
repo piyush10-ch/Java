014b70ae6bac0b24087d70d6fd4979fb98107295
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-10-03 10:21:03 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,14 @@
+public class Typecasting {
+    public static void main(String[]args){
+        int m = 45;
+        double mydouble = m;
+        double n = 98.45;
+        int y = (int)n;
+        System.out.println(m);
+        System.out.println(mydouble); //widening casting
+        System.out.println(n);
+        System.out.println(y);// narrowing casting
+    
+    }
+    
+}
