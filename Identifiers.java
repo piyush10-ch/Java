70a397df122d7dcbfc526e2c6e229dc361f16585
piyush10-ch <68559475+piyush10-ch@users.noreply.github.com>
2021-10-03 10:20:31 +0530

    Add files via upload

---
+++ Identifiers.java
@@ -0,0 +1,8 @@
+public class Identifiers{
+    public static void main(String[] args){
+        int minutesperhour = 60;
+        int m = 60;
+        System.out.println(minutesperhour);
+        System.out.println(m);
+    }
+}
